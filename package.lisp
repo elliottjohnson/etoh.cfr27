32244918170baaf6b102d4fffd1871d5ea180094
Elliott Johnson <elliott@elliottjohnson.net>
2017-05-29 09:44:54 -0700

    Exporting true-percent-proof.

--- package.lisp
+++ package.lisp
@@ -2,6 +2,15 @@
 
 (defpackage #:etoh.cfr27
   (:use #:cl #:unit-formulas #:etoh.units #:local-time #:prove #:prove.suite)
-  (:nicknames #:cfr27))
+  (:nicknames #:cfr27)
+
+  (:export
+
+
+   ;; part 30
+   #:true-percent-proof ; hydrometer correction using table 30.61
+   
+   ))
+
 
 
