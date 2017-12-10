c5362eaaacd83d962e889e8061de26c00538810c
Elliott Johnson <elliott@elliottjohnson.net>
2017-12-10 12:12:27 -0800

    Incremented the timestamp to 2017/12/07.
    Verified that implemented sections have not been modified.

--- etoh.cfr27.lisp
+++ etoh.cfr27.lisp
@@ -4,7 +4,7 @@
 
 (defparameter +ttb-website-url+ "http://www.ttb.gov")
 (defparameter *cfr27-version-timestamp*
-  (encode-timestamp 0 0 0 0 7 6 2017)
+  (encode-timestamp 0 0 0 0 7 12 2017)
   "A timestamp representing the latest version this software relates to.")
 
 (defvar *site-configuration-lazy-load-tables* t
